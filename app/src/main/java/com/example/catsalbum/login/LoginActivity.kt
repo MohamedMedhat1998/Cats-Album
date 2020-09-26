550acd588d940b50c55850ad012ecd77425554d2
MohamedMedhat <abomed7at55@gmail.com>
2020-09-26 16:09:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/catsalbum/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.catsalbum.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.catsalbum.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
